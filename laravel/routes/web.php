7c67c72ebe0719223acd1242b4f8137430316877
Temka <artemijkaspirskij68@gmail.com>
2026-05-18 11:30:14 +0000

    обнова

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,7 +1,19 @@
 <?php
 
+use App\Http\Controllers\LoginController;
+use App\Http\Controllers\LogoutController;
+use App\Http\Controllers\RegisterController;
+use Illuminate\Auth\Events\Login;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
-});
+})->name('dashboard');
+
+Route::get('/register', [RegisterController::class, 'show'])->name('register');
+Route::post('/register', [RegisterController::class, 'store']);
+
+Route::get('/login', [LoginController::class, 'show'])->name('login');
+Route::post('/login', [LoginController::class, 'store']);
+
+Route::post('/logout',LogoutController::class)->name('logout');
